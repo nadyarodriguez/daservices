b5803494bbbb16ea71e4eeaee90592a823befbae
nadyarodriguez <nrodriguez@clinicalmind.com>
2023-07-17 15:30:00 -0600

    styling

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ const button = document.getElementById("sl_button");
 
 button.addEventListener("click", function () {
   console.log("Text to be printed.");
-  document.getElementById(
+  const textElement = (document.getElementById(
     "sl_output"
   ).innerHTML = `1. What is my social audience? (e.g. who they follow, demographics, etc.)
     <br>
   2. What needs/wants are patients expressing on social?<br>
   3. Who are the social influencers? <br>
   4. How was my content perceived? <br>
-  5. What type of content will create the best impact?`;
+  5. What type of content will create the best impact?`);
+
+  textElement;
 });
 // button.addEventListener("change", function () {
 //   const button = document.getElementById("sl_button");
@@ -29,6 +31,14 @@ const button2 = document.getElementById("sld_button");
 
 button2.addEventListener("click", function () {
   console.log("Text to be printed.");
-  document.getElementById("sld_output").innerHTML = `Description of the services
+  document.getElementById(
+    "sld_output"
+  ).innerHTML = `We provide automation and a data driven approach to conference related activities​:
+  <br> - Influencer Identification <br>
+  - Brand Monitoring<br>
+  - Event Monitoring<br>
+  - Landscape Analysis<br>
+- Audience Insights<br>
+- Content Strategy Recommendations
     <br>`;
 });
