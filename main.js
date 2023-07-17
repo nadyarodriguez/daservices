ed238e9bf0db82c05c59b45fd6a85bd09a1772bf
nadyarodriguez <nrodriguez@clinicalmind.com>
2023-07-17 16:09:02 -0600

    adding content

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// Social Listening buttons
+
 const button = document.getElementById("sl_button");
 
 button.addEventListener("click", function () {
@@ -13,19 +15,6 @@ button.addEventListener("click", function () {
 
   textElement;
 });
-// button.addEventListener("change", function () {
-//   const button = document.getElementById("sl_button");
-//   console.log("Text to be printed.");
-//   if (button.checked == true) {
-//     document.getElementById(
-//       "sl_output"
-//     ).innerHTML = `Questions to consider:<br> <br>1. What is my social audience? (e.g. who they follow, demographics, etc.)
-//       <br>
-//     2. What needs/wants are patients expressing on social?`;
-//   } else {
-//     document.getElementById("sl_output").innerHTML = "none";
-//   }
-// });
 
 const button2 = document.getElementById("sld_button");
 
@@ -41,4 +30,39 @@ button2.addEventListener("click", function () {
 - Audience Insights<br>
 - Content Strategy Recommendations
     <br>`;
+});
+
+// HCP buttons
+
+const button_hcp = document.getElementById("hcp_button");
+
+button_hcp.addEventListener("click", function () {
+  document.getElementById(
+    "hcp_output"
+  ).innerHTML = `1. Who are the most influential and/or rising star HCP's?
+    <br>
+  2. What are the qualifications or identified individuals?<br>
+  3. Who is the right fir for a particular type of engagement? <br>
+`;
+});
+
+const button_hcpd = document.getElementById("hcpd_button");
+
+button_hcpd.addEventListener("click", function () {
+  console.log("Text to be printed.");
+  document.getElementById(
+    "hcpd_output"
+  ).innerHTML = `Enabling identification of the right experts for the right engagemnets at the right time​.<br>
+  <br> <strong>Based on...</strong><br> - Medical Publication Authorship <br>
+  - Conference Abstract Authorship<br>
+  - Co-Authorship Newtworks<br>
+  - Clinical Trial Investigations<br>
+- Open Payments<br>
+- Committee Memberships<br>
+- Client Data Sources
+    <br>
+    <br> <strong>We determine...</strong>
+    <br> - Top KOLs<br>
+    - Rising star KOLs <br>
+    - Recommendations for engagement fit`;
 });
